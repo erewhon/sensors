be17297f454a7fcede8a86b614df77576d82baef
Steven Byrnes <steven@byrnes.org>
2021-10-10 00:32:33 -0500

    script to install dependencies on Mac

---
+++ scripts/install-mac.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pip3 install adafruit-blinka \
+             adafruit-circuitpython-amg88xx \
+             adafruit-circuitpython-bmp280 \
+             adafruit-circuitpython-ccs811 \
+             adafruit-circuitpython-ltr390 \
+             adafruit-circuitpython-pm25 \
+             adafruit-circuitpython-scd30 \
+             adafruit-platformdetect \
+             adafruit-python-shell \
+             adafruit-io \
+             hidapi
